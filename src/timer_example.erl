2ae04409fc3f1b83bfb0309ffb23ea6e6a19aeb0
hyc-shayu <hyc_email555@163.com>
2019-12-06 21:37:34 +0800

    modify from timer to my_timer tool

---
+++ src/timer_example.erl
@@ -0,0 +1,7 @@
+%%%----------------------------------------------------------------------
+%%% Use timer_example:start_link() to link your process if you wish.
+%%% This timer server will register as 'timer_example_server', 
+%%% just append '_server' to the module name.
+%%%----------------------------------------------------------------------
+-module(timer_example).
+-include("my_timer.hrl").
